888cbce41fdd55bf0fe27b52426184664ab22aa5
LividJava <lividjava@gmail.com>
2023-03-03 00:10:13 -0500

    Removed personal config files.

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1,6 +1,6 @@
 // priority: 0
 
-console.info('Hello, World! (You will see this line every time server resources reload)')
+console.info('POGGERS LOGGERS.')
 
 ServerEvents.recipes(event => {
 	// Change recipes here
